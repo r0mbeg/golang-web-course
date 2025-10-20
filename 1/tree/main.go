0ba7280ebd76bc4e3477c84bcf73658416171aeb
Roman <romaproffen@yandex.ru>
2025-10-20 12:35:16 +0300

    feat: sol of 1/tree

--- 1/tree/main.go
+++ 1/tree/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path"
 	"path/filepath"
-	"strings"
 )
 
 func main() {
@@ -18,5 +18,72 @@ func main() {
 	err := dirTree(out, path, printFiles)
 	if err != nil {
 		panic(err.Error())
+	}
+}
+
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	printFilesInDir(out, path, "", printFiles)
+	return nil
+}
+
+func getFileSize(path string) (int64, error) {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return 0, err
+	}
+	return fi.Size(), nil
+}
+
+func getFileSizeStr(path string) string {
+
+	fi, _ := os.Stat(path)
+
+	if fi.IsDir() {
+		return ""
+	}
+
+	bytes, err := getFileSize(path)
+	if err != nil {
+		panic(err)
+	}
+	if bytes == 0 {
+		return " (empty)"
+	}
+	return fmt.Sprintf(" (%db)", bytes)
+}
+
+func printFilesInDir(out io.Writer, dir, prefix string, printFiles bool) {
+	initFiles, _ := os.ReadDir(dir)
+
+	files := make([]os.DirEntry, 0, len(initFiles))
+
+	for _, file := range initFiles {
+		if file.IsDir() {
+			files = append(files, file)
+		} else if printFiles {
+			files = append(files, file)
+		}
+	}
+
+	for i, file := range files {
+
+		name := file.Name()
+		parent := filepath.Join(dir, name)
+		branch := "├"
+		newPrefix := prefix + "│\t"
+
+		if i == len(files)-1 {
+			branch = "└"
+			newPrefix = prefix + "\t"
+		}
+
+		if !file.IsDir() && printFiles {
+			name = name + getFileSizeStr(parent)
+		}
+
+		out.Write([]byte(fmt.Sprintf("%s%s───%s\n", prefix, branch, name)))
+
+		printFilesInDir(out, path.Join(dir, file.Name()), newPrefix, printFiles)
+
 	}
 }
